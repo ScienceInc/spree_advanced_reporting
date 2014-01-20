4b9b6071e61fa8e41f6e6e9d93cd22b1a49a6e35
Yazhini <yazhini.2009@gmail.com>
2014-01-20 09:39:12 -0800

    Minor code rearrangement.

--- lib/spree/advanced_report/all_report.rb
+++ lib/spree/advanced_report/all_report.rb
@@ -1,10 +1,4 @@
 class Spree::AdvancedReport::AllReport < Spree::AdvancedReport
-  attr_accessor :customers
-
   def initialize(params)
-    super(params)
-    params[:includes] ||= {}
-    search = Spree::User.includes(params[:includes]).search(params[:search])
-    self.customers = search.result
   end
 end
--- lib/spree/advanced_report/all_report/store_credits.rb
+++ lib/spree/advanced_report/all_report/store_credits.rb
@@ -8,10 +8,10 @@ class Spree::AdvancedReport::AllReport::StoreCredits < Spree::AdvancedReport::Al
   end
 
   def initialize(params)
-    params[:includes] = :store_credits
     super(params)
 
     self.ruportdata = Table(%w[id email name remaining\ amount])
+    customers = Spree::User.includes(:store_credits)
     customers.each do |customer|
       remaining_amount = customer.store_credits.inject(0) { |sum, store_credit| sum += store_credit.remaining_amount }
       if remaining_amount > 0
